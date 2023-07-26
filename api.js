cb796134bfba169a254094cf70bc23a043635845
Obizzle <obilo1kwo@gmail.com>
2023-07-26 08:26:46 +0100

    Refactored firebase, readded <meta />

--- api.js
+++ api.js
@@ -1,3 +1,67 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs, doc, getDoc, query, where } from "firebase/firestore/lite"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcbL-WpErrfDMb6L-2xfhJaBUTe07QvO0",
+  authDomain: "bizl-van-life.firebaseapp.com",
+  projectId: "bizl-van-life",
+  storageBucket: "bizl-van-life.appspot.com",
+  messagingSenderId: "936921608721",
+  appId: "1:936921608721:web:15889b1900c4d921a8b7b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+// Refactoring the fetching functions below
+
+
+const vansCollectionRef = collection(db, "vans")
+
+
+
+
+
+
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    console.log(vans);
+    return vans;
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+
+
+
+/*
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
@@ -11,7 +75,9 @@ export async function getVans(id) {
     const data = await res.json()
     return data.vans
 }
+*/
 
+/*
 export async function getHostVans(id) {
     const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
     const res = await fetch(url)
@@ -25,6 +91,8 @@ export async function getHostVans(id) {
     const data = await res.json()
     return data.vans
 }
+*/
+
 
 export async function loginUser(creds) {
     const res = await fetch("/api/login",
